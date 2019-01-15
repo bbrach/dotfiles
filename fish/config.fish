186268dd2bd88515910560cbcd11b1c64d2a8d21
Boris Brach <bbrach@users.noreply.github.com>
2019-01-15 07:49:45 +0100

    minor install script changes

--- fish/config.fish
+++ fish/config.fish
@@ -5,5 +5,6 @@ alias ....="cd ../../../"
 alias .....="cd ../../../../"
 alias g="git.exe"
 alias e.="e ."
+alias cd.. "cd .."
 setenv EDITOR nvim
 set fish_greeting
--- installRoot.sh
+++ installRoot.sh
@@ -3,6 +3,6 @@
 #Executes operations which require root permissions
 #TODO add /usr/local/bin
 
-pacman -S --noconfirm neovim termite tmux fish lolcat cowsay sl xclip w3m ffmpegthumbnailer python python-pip trash-cli htop
+pacman -S --noconfirm neovim termite tmux fish lolcat cowsay sl xclip w3m ffmpegthumbnailer python python-pip trash-cli htop feh psmisc
 
 ln -s /usr/bin/nvim /usr/bin/n
